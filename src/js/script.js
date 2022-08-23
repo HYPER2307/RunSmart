e816fd3afcee3f2ce840aad9e8d439e7890f4518
Adrian <adrintik@gmail.com>
2022-08-23 12:38:17 +0300

    add modal for and mobile adaptetion

--- src/js/script.js
+++ src/js/script.js
@@ -35,4 +35,21 @@ $(document).ready(function(){
 
       toggelSlide('.catalog-item__link');
       toggelSlide('.catalog-item__back');
+
+      //modal
+
+      $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+      });
+      $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+      });
+      
+
+      $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+      });
   });
